07b17702b3cf7b216e028d047f91093b5560623b
henriquesm <henriques.m@terra.com.br>
2016-01-04 17:51:54 -0200

    Criação
    Criação de Leitor de RSS

---
+++ RssReader/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace RssReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Tocar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabPodCast = new System.Windows.Forms.TabPage();
+            this.dgPodCast = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.tabPodCast.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPodCast)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPodCast);
+            this.tabControl1.Location = new System.Drawing.Point(42, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(774, 510);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(766, 484);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Lista";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(214, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(302, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Tocar});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 136);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(760, 345);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Tocar
+            // 
+            this.Tocar.HeaderText = "Tocar";
+            this.Tocar.Name = "Tocar";
+            this.Tocar.ReadOnly = true;
+            this.Tocar.Text = "Tocar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Consultar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.axWindowsMediaPlayer1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(766, 484);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Player";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(56, 48);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(608, 376);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // tabPodCast
+            // 
+            this.tabPodCast.Controls.Add(this.dgPodCast);
+            this.tabPodCast.Location = new System.Drawing.Point(4, 22);
+            this.tabPodCast.Name = "tabPodCast";
+            this.tabPodCast.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPodCast.Size = new System.Drawing.Size(766, 484);
+            this.tabPodCast.TabIndex = 2;
+            this.tabPodCast.Text = "Cadastrar PodCast";
+            this.tabPodCast.UseVisualStyleBackColor = true;
+            // 
+            // dgPodCast
+            // 
+            this.dgPodCast.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPodCast.Location = new System.Drawing.Point(36, 47);
+            this.dgPodCast.Name = "dgPodCast";
+            this.dgPodCast.Size = new System.Drawing.Size(694, 208);
+            this.dgPodCast.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 630);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.tabPodCast.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPodCast)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.DataGridViewButtonColumn Tocar;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabPage tabPodCast;
+        private System.Windows.Forms.DataGridView dgPodCast;
+    }
+}
+
---
+++ RssReader/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Xml.Linq;
+using System.IO;
+using System.Reflection;
+
+namespace RssReader
+{
+    public partial class Form1 : Form
+    {
+        public string xml = @"PodCast.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), xml);
+
+            XElement root = XElement.Load(path);
+
+            List<podCast> links = new List<podCast>();
+
+            links = (from el in root.Elements("Podcast")
+                     select (new podCast
+                    {
+                        nome = (string)el.Element("nome"),
+                        link = (string)el.Element("link")
+                    })).ToList();
+
+
+            comboBox1.DataSource = new BindingSource(links, null);
+            comboBox1.DisplayMember = "nome";
+            comboBox1.ValueMember = "link";
+
+            comboBox1.Update();
+        }
+
+        public void carregaCombo()
+        {
+            DataSet ds = new DataSet();
+
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), xml);
+
+            ds.ReadXml(path);
+
+            dgPodCast.DataSource = ds.Tables[0];
+
+            Dictionary<string, string> List = new Dictionary<string, string>();
+
+            foreach (DataRow item in ds.Tables[0].Rows)
+            {
+                List.Add(item[0].ToString().Trim(), item[1].ToString().Trim());
+            }
+
+
+
+            comboBox1.DataSource = new BindingSource(List, null);
+            comboBox1.DisplayMember = "Value";
+            comboBox1.ValueMember = "Key";
+
+            comboBox1.Update();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string value = ((RssReader.podCast)(comboBox1.SelectedItem)).link;
+
+            List<arquivo> list = new List<arquivo>();
+            string url = value;
+
+            //https://msdn.microsoft.com/pt-br/library/system.xml.xmlreader.create(v=vs.110).aspx
+            XmlReader reader = XmlReader.Create(url);
+
+            //https://msdn.microsoft.com/pt-br/library/bb515814(v=vs.110).aspx
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+            reader.Close();
+
+            foreach (SyndicationItem item in feed.Items)
+            {
+                foreach (var link in item.Links.ToList().Where(x => x.MediaType == "audio/mpeg"))
+                {
+                    if (link.Uri != null)
+                    {
+                        list.Add(new arquivo
+                        {
+                            subject = item.Title != null ? item.Title.Text : string.Empty,
+                            summary = item.Summary != null ? item.Summary.Text : string.Empty,
+                            dataPublicacao = item.PublishDate != null ? item.PublishDate.ToString("dd/MM/yyyy HH:mm") : DateTime.MinValue.ToString("dd/MM/yyyy HH:mm"),
+                            PodCast = link.Uri.ToString()
+                        });
+                    }
+
+                }
+
+            }
+
+            XDocument xDoc = XDocument.Load(xml);
+
+            foreach (arquivo item in list)
+            {
+                XElement srcTree = new XElement("arquivo",
+                          new XElement("subject", item.subject),
+                          new XElement("summary", item.summary),
+                          new XElement("dataPublicacao", item.dataPublicacao),
+                          new XElement("PodCast", item.PodCast)
+                          );
+            }
+
+
+
+            dataGridView1.DataSource = list;
+
+            axWindowsMediaPlayer1.URL = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string boga = string.Empty;
+            boga = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+
+            if (!string.IsNullOrEmpty(boga))
+            {
+                //https://msdn.microsoft.com/pt-br/library/windows/desktop/dd562470(v=vs.85).aspx
+                axWindowsMediaPlayer1.URL = boga;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+
+    public class arquivo
+    {
+        public string subject { get; set; }
+        public string summary { get; set; }
+        public string dataPublicacao { get; set; }
+        public string PodCast { get; set; }
+    }
+    public class podCast
+    {
+        public podCast()
+        {
+            arquivos = new List<arquivo>();
+        }
+        public string nome { get; set; }
+        public string link { get; set; }
+        public List<arquivo> arquivos { get; set; }
+    }
+}
