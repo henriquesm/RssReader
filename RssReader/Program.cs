e4126020ccc5551b0c69a204f9786afab44b0a9b
Henrique <henriques.m@terra.com.br>
2017-02-21 15:13:12 -0300

    teste
    teste

--- RssReader/Program.cs
+++ RssReader/Program.cs
@@ -17,6 +17,9 @@ namespace RssReader
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+            //teste git
         }
     }
 }
